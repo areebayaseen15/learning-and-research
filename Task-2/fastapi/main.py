e1ea2c4a215ef71556df9a4ed318128e4080d136
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2025-05-08 01:44:23 +0500

    Add files via upload

---
+++ Task-2/fastapi/main.py
@@ -0,0 +1,37 @@
+
+import random
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+simple_greet = "Hello World"
+
+greet_in_Diffrent_Lang = [
+     {"language": "English", "message": "Hello World"},
+        {"language": "Spanish", "message": "Hola Mundo"},
+        {"language": "French", "message": "Bonjour le monde"},
+        {"language": "German", "message": "Hallo Welt"},
+        {"language": "Italian", "message": "Ciao Mondo"},
+        {"language": "Japanese", "message": "こんにちは世界 (Konnichiwa Sekai)"},
+        {"language": "Urdu", "message": "سلام دنیا"},
+        {"language": "Hindi", "message": "नमस्ते दुनिया"},
+        {"language": "Chinese (Simplified)", "message": "你好，世界"},
+        {"language": "Russian", "message": "Привет мир"}
+]
+
+@app.get("/simple_greet")
+def get_message():
+    "Return hello message "
+    return {"greet message:" , simple_greet}
+
+
+@app.get("/greet_in_Diffrent_Lang")
+def get_message():
+    "Return hello messages in different languages"
+    item = random.choice(greet_in_Diffrent_Lang)
+    return {
+        "greet_in_language": item["language"],
+        "message": item["message"]
+    }
